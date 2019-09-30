a6cd650952a051ebbfa6c7648d2a4aa6bdce8d3b
Chun-Chieh Li <ccli8@nuvoton.com>
2019-09-30 16:59:38 +0800

    M2351: Pre-build PSA secure program in formal way
    Pre-build PSA secure program by invocking release.py. This is done formally
    in every mbed-os release.

---
+++ build_psa_tfm_prebuild____release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Configure toolchain path
+export MBED_ARMC6_PATH="C:/Keil_v5/ARM/ARMCLANG_V6.12/bin"
+export MBED_GCC_ARM_PATH="C:/Program Files (x86)/GNU Tools Arm Embedded/8 2019-q3-update/bin"
+
+# Specify toolchain
+TOOLCHAIN="ARMC6"
+# Specify target
+TARGET="NU_PFM_M2351_S"
+
+# Build in mbed-os directory
+cd mbed-os
+python ./tools/psa/release.py -m $TARGET -t $TOOLCHAIN
+cd ..
