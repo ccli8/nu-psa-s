1a0e7db50a40200060c34783cbfb8fd8c5bb1e76
Chun-Chieh Li <ccli8@nuvoton.com>
2020-01-14 14:25:55 +0800

    Support IAR

---
+++ build_nopsa____iar.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TOOLCHAIN="IAR"
+TARGET="NU_PFM_M2351_NPSA_S"
+APP_CONFIG="mbed_app_nopsa.json"
+PROFILE="develop"
+TOOLCHAIN_PROFILE=$TOOLCHAIN-`echo $PROFILE | tr '[a-z]' '[A-Z]'`
+
+# NuMaker-mbed-TZ-secure-example.bin/hex
+BUILD_NAME="NuMaker-mbed-TZ-secure-example"
+# Remove BUILD directory for clean build
+rm -rdf BUILD
+# Build
+mbed compile -m $TARGET -t $TOOLCHAIN \
+--app-config $APP_CONFIG \
+--profile $PROFILE
+# Save built code
+BUILD_DIR=BUILD/$TARGET/$TOOLCHAIN_PROFILE
+SAVE_DIR=TARGET_IGNORE/$TARGET/$TOOLCHAIN_PROFILE/$BUILD_NAME
+mkdir -p $SAVE_DIR
+cp $BUILD_DIR/$BUILD_NAME.hex $SAVE_DIR
+cp $BUILD_DIR/cmse_lib.o $SAVE_DIR
---
+++ nonsecure_call_reset_handler.c
@@ -0,0 +1,17 @@
+#if TFM_LVL == 0
+
+#include <arm_cmse.h>
+#include <stdint.h>
+
+typedef void (*ns_reset_handler_call) (void) __attribute__((cmse_nonsecure_call));
+
+void call_ns_reset_handler(uintptr_t ns_reset_handler_addr)
+{
+    ns_reset_handler_call ns_reset_handler = (ns_reset_handler_call) ns_reset_handler_addr;
+
+    ns_reset_handler = cmse_nsfptr_create(ns_reset_handler); 
+
+    ns_reset_handler();
+}
+
+#endif
